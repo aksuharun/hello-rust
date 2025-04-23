fd3523bff9870528596b8a6bcc3713475a004b35
aksuharun <harun16aksu@gmail.com>
2025-04-23 12:01:30 +0300

    Initial commit
    First Rust programs written while learning from the official Rust Book. Streamed the process live on YouTube: https://www.youtube.com/live/IGOwjAnV7LU?si=OFyv32NIp64MebWy

---
+++ rust-project-1/guessing-game/src/main.rs
@@ -0,0 +1,38 @@
+use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
+
+
+fn main() {
+  println!("Guessing Game!");
+  
+  let number = rand::thread_rng().gen_range(1..=10);
+  
+  loop {
+    println!("Please input your guess between 1 and 10:");
+    let mut guess = String::new();
+    io::stdin()
+      .read_line(&mut guess)
+    .expect("Failed to read line");
+
+    let guess: u8 = match guess.trim().parse() {
+      Ok(num) => num,
+      Err(_) => {
+        print!("You must type a number! ");
+        continue;
+      }
+    };
+
+    println!("You guessed: {guess}");
+
+    match guess.cmp(&number) {
+      Ordering::Less => println!("Too small!"),
+      Ordering::Greater => println!("Too big!"),
+      Ordering::Equal => {
+        println!("You win!");
+        break;
+      }
+    }
+  }
+}
+
